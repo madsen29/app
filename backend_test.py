fbffb7f6f28521611f1ed41862edf354f8d48b05
Jake Madsen <madsen29@gmail.com>
2025-07-16 17:07:38 +0000

    auto-commit for 89ba78ea-be84-4434-8125-71ae0d1d6790

--- backend_test.py
+++ backend_test.py
@@ -831,5 +831,5 @@ class BackendTester:
 
 if __name__ == "__main__":
     tester = BackendTester()
-    success = tester.run_all_tests()
+    success = tester.run_review_request_tests()
     sys.exit(0 if success else 1)
