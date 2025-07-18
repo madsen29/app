8369d40e748abf0616030aa8b4dcaf9975bd4304
Jake Madsen <madsen29@gmail.com>
2025-07-18 00:20:17 +0000

    auto-commit for a0bf2931-ffed-4b97-9455-01b4bdbc0c6d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import { BrowserMultiFormatReader } from '@zxing/browser';
 import { BarcodeFormat } from '@zxing/library';
 import { FiCamera, FiChevronRight, FiPackage, FiBox, FiFolder, FiFile, FiX } from 'react-icons/fi';
+import { useAuth } from './AuthContext';
+import AuthWrapper from './AuthWrapper';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
