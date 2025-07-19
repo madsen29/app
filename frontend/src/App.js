b2e20072888b3cfa0918eeba2789ae57e0e5ef95
Jake Madsen <madsen29@gmail.com>
2025-07-19 16:05:21 +0000

    auto-commit for fc8a398a-7931-445c-9c38-a07e4deae96c

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2812,7 +2812,7 @@ function App() {
             {/* Sender Information */}
             <div className="business-entity-group">
               <div className="flex justify-between items-center mb-4">
-                <h4>Sender Information</h4>
+                <h4 className="m">Sender Information</h4>
                 <button
                   type="button"
                   onClick={() => openLocationSelector('sender')}
