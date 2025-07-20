4e044c788a95e7ca029a4608e3e1c0cf6712479f
Jake Madsen <madsen29@gmail.com>
2025-07-20 04:28:19 +0000

    auto-commit for dc303e0d-15fb-4437-81f7-9007efa3ceab

--- frontend/src/ProjectDashboard.js
+++ frontend/src/ProjectDashboard.js
@@ -42,12 +42,12 @@ const ProjectDashboard = ({ onSelectProject, onCreateProject, onLogout }) => {
   const calculateProjectTotals = (config) => {
     if (!config) return { totalCases: 0, totalInnerCases: 0, totalItems: 0 };
     
-    const numberOfSscc = config.number_of_sscc || 0;
-    const casesPerSscc = config.cases_per_sscc || 0;
-    const itemsPerCase = config.items_per_case || 0;
-    const useInnerCases = config.use_inner_cases || false;
-    const innerCasesPerCase = config.inner_cases_per_case || 0;
-    const itemsPerInnerCase = config.items_per_inner_case || 0;
+    const numberOfSscc = config.numberOfSscc || 0;
+    const casesPerSscc = config.casesPerSscc || 0;
+    const itemsPerCase = config.itemsPerCase || 0;
+    const useInnerCases = config.useInnerCases || false;
+    const innerCasesPerCase = config.innerCasesPerCase || 0;
+    const itemsPerInnerCase = config.itemsPerInnerCase || 0;
     
     let totalCases = 0;
     let totalInnerCases = 0;
