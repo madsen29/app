0984b0e9660fa60caaf3fe2a4eb7d5ff6331786b
Jake Madsen <madsen29@gmail.com>
2025-09-05 21:15:27 +0000

    auto-commit for 9cd8ace7-8b17-4da6-a82f-6f808e17a209

--- frontend/src/SimpleScandItScanner.js
+++ frontend/src/SimpleScandItScanner.js
@@ -5,6 +5,8 @@
 
 export class SimpleScandItScanner {
   constructor(licenseKey) {
+    console.log('🔑 Scanner constructor - License key length:', licenseKey?.length);
+    console.log('🔑 Scanner constructor - License key preview:', licenseKey?.substring(0, 50) + '...');
     this.licenseKey = licenseKey;
     this.scanner = null;
     this.onScanCallback = null;
