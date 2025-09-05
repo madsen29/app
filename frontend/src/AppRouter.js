3f3886e05a744c5bb001c6134ff699b37503e826
Jake Madsen <madsen29@gmail.com>
2025-09-05 18:54:57 +0000

    auto-commit for 7296b5ac-91db-4980-b3ac-80efc9665946

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AuthProvider } from './AuthContext';
 import App from './App';
 import Admin from './Admin';
+import ScannerTest from './ScannerTest';
 
 const AppRouter = () => {
   // Simple routing based on URL path
