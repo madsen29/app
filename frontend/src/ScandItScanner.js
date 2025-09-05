ac8ffd76d8b301ae92bff9f52f186baf34e22ad6
Jake Madsen <madsen29@gmail.com>
2025-09-05 17:35:31 +0000

    auto-commit for 6d4b967a-71b2-42c0-96a3-78aa7f2b452a

--- frontend/src/ScandItScanner.js
+++ frontend/src/ScandItScanner.js
@@ -92,7 +92,7 @@ export class ScandItScanner {
       settings.scanIntention = this.SDCBarcode.ScanIntention.Smart; // Smart scanning intention
 
       // Create SparkScan instance
-      this.sparkScan = new this.SDCBarcode.SparkScan(this.context, settings);
+      this.sparkScan = this.SDCBarcode.SparkScan.forSettings(settings);
 
       // Configure SparkScan view settings
       const viewSettings = new this.SDCBarcode.SparkScanViewSettings();
