301b71a7006932761aaa6fd82bbc97b22230f4e8
Jake Madsen <madsen29@gmail.com>
2025-09-05 17:35:55 +0000

    auto-commit for 818d5307-48b7-480f-9f5c-f6a522177a7e

--- frontend/src/ScandItScanner.js
+++ frontend/src/ScandItScanner.js
@@ -161,7 +161,7 @@ export class ScandItScanner {
       settings.enableSymbology(this.SDCBarcode.Symbology.DataMatrix);
 
       // Create BarcodeBatch instance
-      this.barcodeBatch = new this.SDCBarcode.BarcodeBatch(this.context, settings);
+      this.barcodeBatch = this.SDCBarcode.BarcodeBatch.forContext(this.context, settings);
 
       // Setup camera
       const cameraSettings = this.SDCBarcode.BarcodeBatch.recommendedCameraSettings;
