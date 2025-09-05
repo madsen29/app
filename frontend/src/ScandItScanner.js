ebf1458d3427159eec8b114fadf212d80da48056
Jake Madsen <madsen29@gmail.com>
2025-09-05 17:06:58 +0000

    auto-commit for 0845a007-1af7-41d1-97cd-2fa33f426d79

--- frontend/src/ScandItScanner.js
+++ frontend/src/ScandItScanner.js
@@ -166,7 +166,7 @@ export class ScandItScanner {
       await this.context.setFrameSource(this.camera);
 
       // Create data capture view
-      this.view = await this.SDCCore.DataCaptureView.forContext(this.context);
+      this.view = new this.SDCCore.DataCaptureView(this.context);
       this.view.connectToElement(containerElement);
 
       // Add batch overlay for visual feedback
